801e935d96ef146203b1ecdedbcbd77b74c6d3b5
Zicka67 <art.book@hotmail.fr>
2023-01-12 09:45:26 +0100

    Ajout des getter / setter

--- Hotel.php
+++ Hotel.php
@@ -3,20 +3,66 @@
 class Hotel
 {
 
-private $name;
-private $room;
-private $adress;    
-private $reservation;
+    private $name; //
+    private $room;
+    private $adress; //
+    private $reservation;
 
-public function __construct($name, $room, $adress)
-{
-    $this->name = $name;
-}
+    public function __construct($name, $adress)
+    {
+        $this->name = $name;
+        $this->room = []; // tab ?
+        $this->adress = $adress;
+        $this->reservation = [];
+    }
 
+    // ******** GETTER / SETTER *********
 
+    public function getName()
+    {
+        return $this->name;
+    }
 
+    public function setName($name)
+    {
+        $this->name = $name;
 
+        return $this;
+    }
 
+    public function getRoom()
+    {
+        return $this->room;
+    }
 
+    public function setRoom($room)
+    {
+        $this->room = $room;
 
+        return $this;
+    }
+
+    public function getAdress()
+    {
+        return $this->adress;
+    }
+
+    public function setAdress($adress)
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getReservation()
+    {
+        return $this->reservation;
+    }
+
+    public function setReservation($reservation)
+    {
+        $this->reservation = $reservation;
+
+        return $this;
+    }
 }
