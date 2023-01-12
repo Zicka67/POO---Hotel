1faa9b298cb373b9856894be1ac3da62502d7313
Zicka67 <art.book@hotmail.fr>
2023-01-12 09:15:09 +0100

    Création du fichier class hotel

---
+++ Hotel.php
@@ -0,0 +1,22 @@
+<?php
+
+class Hotel
+{
+
+private $name;
+private $room;
+private $adress;    
+private $reservation;
+
+public function __construct($name, $room, $adress)
+{
+    $this->name = $name;
+}
+
+
+
+
+
+
+
+}
