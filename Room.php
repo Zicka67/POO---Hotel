27101ae98f6322829c78132c64924d21c1c818fd
Zicka67 <art.book@hotmail.fr>
2023-01-13 17:05:49 +0100

    Terminé

--- Room.php
+++ Room.php
@@ -2,7 +2,7 @@
 
 class Room
 {
-
+    
     private string $name;
     private int $price;
     private int $nbBed;
@@ -10,115 +10,114 @@ class Room
     private $wifi; 
     private $disponibility; 
     private array $reservation;
-
+    
     public function __construct($name, $price, $nbBed, $hotel)
     {
         $this->name = $name;
         $this->price = $price;
         $this->nbBed = $nbBed;
-
+        
         $this->hotel = $hotel;
         $this->hotel->addRoom($this); // A ne plus oublier ! (plus d'infos pour mieux comprendre)
-
+        
         $this->disponibility =true;
         $this->reservation = [];
         $this->wifi = false;
     }
-
+    
     // ******** GETTER / SETTER *********
-
+    
     public function getName()
     {
         return $this->name;
     }
-
+    
     public function setName($name)
     {
         $this->name = $name;
-
+        
         return $this;
     }
-
+    
     public function getPrice()
     {
         return $this->price;
     }
-
+    
     public function setPrice($price)
     {
         $this->price = $price;
-
+        
         return $this;
     }
-
+    
     public function getNbBed()
     {
         return $this->nbBed;
     }
-
-
+    
     public function setNbBed($nbBed)
     {
         $this->nbBed = $nbBed;
-
+        
         return $this;
     }
-
+    
     public function getHotel()
     {
         return $this->hotel;
     }
-
+    
     public function setHotel($hotel)
     {
         $this->hotel = $hotel;
-
+        
         return $this;
     }
-
+    
     public function getWifi()
     {
         return $this->wifi;
     }
-
+    
     public function setWifi($wifi)
     {
         $this->wifi = $wifi;
-
+        
         return $this;
     }
-
+    
     public function getDisponibility() //countReservedRooms
     {
         return $this->disponibility;
     }
-
+    
     public function setDisponibility($disponibility)
     {
         $this->disponibility = $disponibility;
-
+        
         return $this;
     }
-
+    
     // ***** ADDRESERVATION *****
-
+    
     public function addReservation($reservation)
     {
         $this->reservation[] = $reservation;
     }
-
+    
     // ***** WIFISTATUS *****
-
+    
     public function statusWifi()
     {
         if($this->disponibility == true)
-            $this->disponibility = false;
+        $this->disponibility = false;
         else {
             $this->disponibility = true;
         }
     }
-
-  
+    
+    
 }
 
 ?>
