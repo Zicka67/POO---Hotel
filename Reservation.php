3917445bbc80bc9d4d0a63acb90fced3fbcc4715
Zicka67 <art.book@hotmail.fr>
2023-01-13 18:07:35 +0100

    Ajout et modification des commentaires

--- Reservation.php
+++ Reservation.php
@@ -12,18 +12,16 @@ class Reservation
     {
         $this->dtStart = $dtStart;
         $this->dtEnd = $dtEnd;
-
+        
         $this->hotel = $hotel;
         $this->hotel->addReservation($this);
+        
         $this->user = $user;
         $this->user->addReservation($this);
         
         $this->room = $room;
         $this->room->addReservation($this);
-        $this->room->statusWifi($this);
-        // $this->room->getHotel()->addReservation($this); // On appel getHotel() de l'objet $this->room, et donc renvoie l'objet hotel à la room actuel.
-        // on appel addReservation() sur l'objet getHotel avec comme argument $this(qui est l'objet de réservation actuel)
-        //Cela ajoute l'objet de réservation actuel ($this) au tab de reservation de l'hotel.
+        $this->room->disponibilityEtat($this);
     }
     
     // ******** GETTER / SETTER *********
@@ -79,5 +77,12 @@ class Reservation
     public function getHotel()
     {
         return $this->hotel;
+    }
+
+    public function setHotel($hotel): self
+    {
+        $this->hotel = $hotel;
+        
+        return $this;
     }
 }
