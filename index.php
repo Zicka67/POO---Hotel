7f743891861c2257540dbbcf80e01f1e42d95868
Zicka67 <art.book@hotmail.fr>
2023-01-13 18:07:13 +0100

    Test de setwifi ( fonctionne )

--- index.php
+++ index.php
@@ -35,32 +35,32 @@ $room1 = new Room("Chambre 1", 120, 2, $hotel1);
 $room2 = new Room("Chambre 2", 120, 2, $hotel1);
 $room3 = new Room("Chambre 3", 120, 2, $hotel1);
 $room4 = new Room("Chambre 4", 120, 2, $hotel1);
-
 $room16 = new Room("Chambre 16", 300, 2, $hotel1);
 $room17 = new Room("Chambre 17", 300, 2, $hotel1);
 $room18 = new Room("Chambre 18", 300, 2, $hotel1);
 $room19 = new Room("Chambre 19", 300, 2, $hotel1);
 
 $reservation1 = new Reservation($user2, "01-01-2021", "01-01-2021", $room17, $hotel1);
+
 $reservation2 = new Reservation($user1, "11-03-2021", "15-03-2021", $room3, $hotel1);
 $reservation3 = new Reservation($user1, "01-04-2021", "17-04-2021", $room4, $hotel1);
 
-$room3->setWifi(true);
+$room16->setWifi(true);
+$room17->setWifi(true);
+$room18->setWifi(true);
+$room19->setWifi(false);
 
 // var_dump($hotel1->getHotelReservation());
 // echo "<br><br>";
-// var_dump($room3);
 $hotel1->getHotelInfos();
-// $hotel2->getHotelInfos();
 echo "<br><br>";
 $hotel1->getHotelReservation();
 echo "<br><br>";
 $hotel2->getHotelReservation();
 echo "<br><br>";
-// $hotel1->roomResumed();
 $user1->getUserReservation();
 
-$hotel1->tabResumedRooms();
+$hotel1->tabResumedRooms(); // Seulement le user2 peut reserver des chambres, ne fonctionne pas avec le user1. Donc uniquement la 17 est affiché en reservée
 
 ?>
 
