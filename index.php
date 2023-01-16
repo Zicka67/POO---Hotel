da81b445430552e4f1e98bc2ee9cc6bb3b40db5f
Tameem <th468737@gmail.com>
2023-01-16 10:03:15 +0100

    Ajout du link de fontawesome

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="styles.css">
 
 <title>POO HOTEL</title>
@@ -39,26 +39,28 @@ $room18 = new Room("Chambre 18", 300, 2, $hotel1);
 $room19 = new Room("Chambre 19", 300, 2, $hotel1);
 
 $reservation1 = new Reservation($user2, "01-01-2021", "01-01-2021", $room17, $hotel1);
-
 $reservation2 = new Reservation($user1, "11-03-2021", "15-03-2021", $room3, $hotel1);
 $reservation3 = new Reservation($user1, "01-04-2021", "17-04-2021", $room4, $hotel1);
+// $reservation4 = new Reservation($user2, "01-04-2021", "17-04-2021", $room18, $hotel1);
 
 $room16->setWifi(true);
 $room17->setWifi(true);
 $room18->setWifi(true);
 $room19->setWifi(false);
 
-// var_dump($hotel1->getHotelReservation());
+// var_dump($room16->getDisponibility());
 // echo "<br><br>";
 $hotel1->getHotelInfos();
-echo "<br><br>";
+echo "<br><br><br>";
 $hotel1->getHotelReservation();
-echo "<br><br>";
+echo "<br><br><br>";
 $hotel2->getHotelReservation();
-echo "<br><br>";
+echo "<br><br><br>";
 $user1->getUserReservation();
-echo "<br><br>";
+$user2->getUserReservation();
+echo "<br><br><br>";
 $hotel1->tabResumedRooms(); // Seulement le user2 peut reserver des chambres, ne fonctionne pas avec le user1. Donc uniquement la 17 est affiché en reservée
+
 
 ?>
 
